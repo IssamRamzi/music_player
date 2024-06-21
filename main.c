75a0abf756af5240f5b79a68bd0ed7e9ce29b064
Issam <iss.benahmed@outlook.com>
2024-06-21 17:49:06 +0100

    Next and previous music working

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #define MAX_MUSIC 256
 #define MAX_MUSIC_STRING 256
 
+
 void toggleResumePause(Music music)
 {
     if (IsKeyPressed(KEY_SPACE))
@@ -26,23 +27,29 @@ void toggleResumePause(Music music)
     }
 }
 
-void updateMusic(Music *musics, int *i){
+int updateMusic(Music *musics, int *i, int max){
     if(IsKeyPressed(KEY_RIGHT)){
         StopMusicStream(musics[*i]);
-        (*i) +=1;
+        if(*i < max - 1) (*i) +=1;
         PlayMusicStream(musics[*i]);
+        printf("Next Music Playing\n");
+        return 1;
     }
     if(IsKeyPressed(KEY_LEFT)){
         StopMusicStream(musics[*i]);
-        if(i >= 1) (*i) -=1;
+        if(*i >= 1) (*i) -=1;
         PlayMusicStream(musics[*i]);
+        printf("Previous Music Playing\n");
+        return 1;
     }
+    return 0;
 }
 
-int main(void)
-{
+
+
+int main(void){
     InitAudioDevice();
-    InitWindow(800, 600, "Music Player");
+    InitWindow(800, 600, "Spotify");
     SetTargetFPS(60);
 
     Music *musics = (Music*)malloc(sizeof(Music) * MAX_MUSIC);
@@ -51,25 +58,38 @@ int main(void)
     {
         musics_str[i] = (char*)malloc(sizeof(char) * MAX_MUSIC_STRING);
     }
+
     int current = 0;
     addAllQueue(musics, &current, musics_str, "./music");
+    printf("%d\n\n", current);
 
     int toPlay = 0;
 
     SetMusicVolume(musics[toPlay], 0.5f);
     PlayMusicStream(musics[toPlay]);
+    printf("Playing %s\n",musics_str[toPlay]);
     while (!WindowShouldClose())
     {
-        UpdateMusicStream(musics[toPlay]);
         toggleResumePause(musics[toPlay]);
-        updateMusic(musics,&toPlay);
-
+        UpdateMusicStream(musics[toPlay]);
+        char *text = musics_str[toPlay];
+/*         if(updateMusic(musics,&toPlay, current)){
+            printf("New Music Playing !\n");
+        }
+ */
+        updateMusic(musics,&toPlay, current);
         BeginDrawing();
-        ClearBackground(RED);
+        ClearBackground(WHITE);
+        DrawText(text, 0,0,30,BLACK);
+
+        if(IsMusicStreamPlaying(musics[toPlay])){
+            DrawText("Resume",0,80,30,GREEN);
+        }else{
+            DrawText("Pause",0,80,30,RED);
+        }
 
         EndDrawing();
     }
-
     // Free allocated memory
     for (int i = 0; i < MAX_MUSIC; i++)
     {
@@ -79,6 +99,5 @@ int main(void)
     free(musics);
 
     CloseWindow();
-
     return 0;
 }
