bce7b5bdd9e4f28c3e160ed760adad1ace0ee117
Issam <iss.benahmed@outlook.com>
2024-06-23 00:19:41 +0100

    Add status music

--- main.c
+++ main.c
@@ -5,15 +5,15 @@
 #include <raylib.h>
 #include "utils.h"
 
-
 #define MAX_MUSIC 256
 #define MAX_MUSIC_STRING 256
 
-typedef enum {
+typedef enum
+{
     PLAYING,
     PAUSED,
     FINISHED
-}STATUS;
+} STATUS;
 
 void toggleResumePause(Music music)
 {
@@ -32,19 +32,22 @@ void toggleResumePause(Music music)
     }
 }
 
-int updateMusic(Music *musics, int *i, int max){
-    if(IsKeyPressed(KEY_RIGHT) ||
-    (GetMusicTimeLength(musics[*i]) - 0.2f) == GetMusicTimePlayed(musics[*i])
-    ){
+int updateMusic(Music *musics, int *i, int max)
+{
+    if (IsKeyPressed(KEY_RIGHT) ||
+        (GetMusicTimeLength(musics[*i]) - 0.2f) == GetMusicTimePlayed(musics[*i]))
+    {
         StopMusicStream(musics[*i]);
-        if(*i < max - 1) (*i) +=1;
+        if (*i < max - 1)
+            (*i) += 1;
         PlayMusicStream(musics[*i]);
         printf("Next Music Playing\n");
         return 1;
     }
-    if(IsKeyPressed(KEY_LEFT)){
+    if (IsKeyPressed(KEY_LEFT)){
         StopMusicStream(musics[*i]);
-        if(*i >= 1) (*i) -=1;
+        if (*i >= 1)
+            (*i) -= 1;
         PlayMusicStream(musics[*i]);
         printf("Previous Music Playing\n");
         return 1;
@@ -52,18 +55,16 @@ int updateMusic(Music *musics, int *i, int max){
     return 0;
 }
 
-
-
-int main(void){
+int main(void)
+{
     InitAudioDevice();
     InitWindow(800, 600, "Spotify");
     SetTargetFPS(60);
 
-    Music *musics = (Music*)malloc(sizeof(Music) * MAX_MUSIC);
-    char** musics_str = (char**)malloc(sizeof(char*) * MAX_MUSIC);
-    for (int i = 0; i < MAX_MUSIC; i++)
-    {
-        musics_str[i] = (char*)malloc(sizeof(char) * MAX_MUSIC_STRING);
+    Music *musics = (Music *)malloc(sizeof(Music) * MAX_MUSIC);
+    char **musics_str = (char **)malloc(sizeof(char *) * MAX_MUSIC);
+    for (int i = 0; i < MAX_MUSIC; i++){
+        musics_str[i] = (char *)malloc(sizeof(char) * MAX_MUSIC_STRING);
     }
 
     int current = 0;
@@ -74,42 +75,40 @@ int main(void){
 
     SetMusicVolume(musics[toPlay], 0.5f);
     PlayMusicStream(musics[toPlay]);
-    printf("Playing %s\n",musics_str[toPlay]);
-    while (!WindowShouldClose())
-{
-    float current_time = GetMusicTimePlayed(musics[toPlay]);
-    float max_time = GetMusicTimeLength(musics[toPlay]);
+    printf("Playing %s\n", musics_str[toPlay]);
+    while (!WindowShouldClose()){
+        float current_time = GetMusicTimePlayed(musics[toPlay]);
+        float max_time = GetMusicTimeLength(musics[toPlay]);
 
-    toggleResumePause(musics[toPlay]);
-    UpdateMusicStream(musics[toPlay]);
-    char *text = musics_str[toPlay];
+        toggleResumePause(musics[toPlay]);
+        UpdateMusicStream(musics[toPlay]);
+        char *text = musics_str[toPlay];
 
-    updateMusic(musics, &toPlay, current);
-    
-    BeginDrawing();
-    ClearBackground(WHITE);
-    DrawText(text, 0, 0, 30, BLACK);
+        updateMusic(musics, &toPlay, current);
 
-    if (IsMusicStreamPlaying(musics[toPlay]))
-    {
-        DrawText("Resume", 0, 80, 30, GREEN);
+        BeginDrawing();
+        ClearBackground(WHITE);
+        DrawText(text, 0, 0, 30, BLACK);
+
+        if (IsMusicStreamPlaying(musics[toPlay]))
+        {
+            DrawText("Resume", 0, 80, 30, GREEN);
+        }
+        else
+        {
+            DrawText("Pause", 0, 80, 30, RED);
+        }
+
+        char time_info[100];
+        snprintf(time_info, sizeof(time_info), "Current Time: %3.2f / Max Time: %3.2f", current_time, max_time);
+
+        DrawText(time_info, 0, 120, 20, BLACK);
+
+        EndDrawing();
     }
-    else
-    {
-        DrawText("Pause", 0, 80, 30, RED);
-    }
-    
-    char time_info[100];
-    snprintf(time_info, sizeof(time_info), "Current Time: %3.2f / Max Time: %3.2f", current_time, max_time);
-
-    DrawText(time_info, 0, 120, 20, BLACK);
-
-    EndDrawing();
-}
 
     // Free allocated memory
-    for (int i = 0; i < MAX_MUSIC; i++)
-    {
+    for (int i = 0; i < MAX_MUSIC; i++){
         free(musics_str[i]);
     }
     free(musics_str);
